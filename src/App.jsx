c3cc6c90eaf20f261c6b786a677ff8b5e8bf2f12
Roman Tolstopyatov <r.tolstopyatov@gmail.com>
2018-04-11 18:49:23 +0300

    add random button

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Cell from './Cell';
 
 const width = 80;
 const height = 50;
+const density = 0.3;
 
 export default class App extends Component {
   constructor(props) {
@@ -13,7 +14,7 @@ export default class App extends Component {
     for (let i = 0; i < height; i++) {
       curData.push([]);
       for (let j = 0; j < width; j++) {
-        if (Math.random() > 0.3) {
+        if (Math.random() > density) {
           curData[i].push(0);
         } else {
           curData[i].push(1);
@@ -28,6 +29,7 @@ export default class App extends Component {
     this.start = this.start.bind(this);
     this.pause = this.pause.bind(this);
     this.clear = this.clear.bind(this);
+    this.random = this.random.bind(this);
   }
 
   componentDidMount() {
@@ -76,6 +78,14 @@ export default class App extends Component {
     });
   }
 
+  random() {
+    const cells = this.state.data.map(r => r.map(() => +(Math.random() < density)));
+    this.setState({
+      data: cells,
+      counter: 0,
+    });
+  }
+
   render() {
     const buildRow = (r, i) => {
       const row = r.map((v, j) =>
@@ -90,6 +100,7 @@ export default class App extends Component {
         <button onClick={this.start}>Start</button>
         <button onClick={this.pause}>Pause</button>
         <button onClick={this.clear}>Clear</button>
+        <button onClick={this.random}>Random</button>
         <span className="generation">
           generation: <span className="gen_value">{this.state.counter}</span>
         </span>
